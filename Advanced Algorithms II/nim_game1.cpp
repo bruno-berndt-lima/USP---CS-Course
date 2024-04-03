715f59562b1157a066cc786b2f8595d24292b3af
Bruno Berndt Lima <83605421+bruno-berndt-lima@users.noreply.github.com>
2024-04-02 21:04:04 -0300

    Create nim_game1.cpp

---
+++ Advanced Algorithms II/nim_game1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n_tests, n_heaps;
+    cin >> n_tests;
+
+    while(n_tests > 0) {
+        cin >> n_heaps;
+        vector<int> sticks_in_heap(n_heaps);
+        int res = 0, buffer;
+        for (int i = 0; i < n_heaps; i++) {
+            cin >> sticks_in_heap[i];
+            res ^= sticks_in_heap[i];
+        }
+        
+        res == 0 ? cout << "second\n" : cout << "first\n";
+        
+        n_tests--;
+    }
+
+
+    return 0;
+}
